7308c8f185d72891f7edaee4b3c44f768b732a35
Debajyati <ddebajyati@gmail.com>
2024-04-15 23:20:04 +0530

    output of codes in filename.sol
    filename.sol has the output of filename.py

---
+++ numpy-pandas/basics.sol
@@ -0,0 +1,99 @@
+        0       1       2       3  ...      6       7          8     9
+0       1       2       3       4  ...      7       8          9    10
+1  Sandip  Prince  Faisal  Piyush  ...  Arnab  Swappy  Debashish  1729
+2       0      -1       3      10  ...      7       7          9    10
+
+[3 rows x 10 columns]
+                    0       1       2       3  ...      6       7          8     9
+Sl no.              1       2       3       4  ...      7       8          9    10
+Name           Sandip  Prince  Faisal  Piyush  ...  Arnab  Swappy  Debashish  1729
+Attack-Points       0      -1       3      10  ...      7       7          9    10
+
+[3 rows x 10 columns]
+  Sl no.       Name Attack-Points
+3      4     Piyush            10
+4      5     Daykon             5
+6      7      Arnab             7
+7      8     Swappy             7
+8      9  Debashish             9
+9     10       1729            10
+
+  Sl no.       Name Attack-Points
+0      1     Sandip             0
+1      2     Prince            -1
+2      3     Faisal             3
+3      4     Piyush            10
+4      5     Daykon             5
+5      6      Anish             4
+6      7      Arnab             7
+7      8     Swappy             7
+8      9  Debashish             9
+9     10       1729            10
+
+First 7 rows:
+
+  Sl no.    Name Attack-Points
+0      1  Sandip             0
+1      2  Prince            -1
+2      3  Faisal             3
+3      4  Piyush            10
+4      5  Daykon             5
+5      6   Anish             4
+6      7   Arnab             7
+
+Last 7 rows:
+  Sl no.       Name Attack-Points
+3      4     Piyush            10
+4      5     Daykon             5
+5      6      Anish             4
+6      7      Arnab             7
+7      8     Swappy             7
+8      9  Debashish             9
+9     10       1729            10
+New Dataframe:
+
+    Duration          Date  Pulse  Maxpulse  Calories  Double_Duration
+0         60  '2020/12/01'    110       130     409.1              120
+1         60  '2020/12/02'    117       145     479.0              120
+2         60  '2020/12/03'    103       135     340.0              120
+3         45  '2020/12/04'    109       175     282.4               90
+4         45  '2020/12/05'    117       148     406.0               90
+5         60  '2020/12/06'    102       127     300.0              120
+6         60  '2020/12/07'    110       136     374.0              120
+7        450  '2020/12/08'    104       134     253.3              900
+8         30  '2020/12/09'    109       133     195.1               60
+9         60  '2020/12/10'     98       124     269.0              120
+10        60  '2020/12/11'    103       147     329.3              120
+11        60  '2020/12/12'    100       120     250.7              120
+12        60  '2020/12/12'    100       120     250.7              120
+13        60  '2020/12/13'    106       128     345.3              120
+14        60  '2020/12/14'    104       132     379.3              120
+15        60  '2020/12/15'     98       123     275.0              120
+16        60  '2020/12/16'     98       120     215.2              120
+17        60  '2020/12/17'    100       120     300.0              120
+18        45  '2020/12/18'     90       112       NaN               90
+19        60  '2020/12/19'    103       123     323.0              120
+20        45  '2020/12/20'     97       125     243.0               90
+21        60  '2020/12/21'    108       131     364.2              120
+22        45           NaN    100       119     282.0               90
+23        60  '2020/12/23'    130       101     300.0              120
+24        45  '2020/12/24'    105       132     246.0               90
+25        60  '2020/12/25'    102       126     334.5              120
+26        60      20201226    100       120     250.0              120
+27        60  '2020/12/27'     92       118     241.0              120
+28        60  '2020/12/28'    103       132       NaN              120
+29        60  '2020/12/29'    100       132     280.0              120
+30        60  '2020/12/30'    102       129     380.3              120
+31        60  '2020/12/31'     92       115     243.0              120
+
+ merged DataFrame -
+    ID        Name       Opponent
+0   2   Nightmare      Scarecrow
+1   1  Sabretooth  Talia-Al-Ghul 
+
+
+    ID         Player  Attack Power  Updated Power
+0   1  Talia-Al-Ghul         45000          50000
+1   2      Scarecrow         70000          78620
+2   3       Two-face         55000          59560 
+
---
+++ numpy-pandas/series.sol
@@ -0,0 +1,38 @@
+2002    grapefruit
+dtype: object
+        mean  max  min
+London  23.5   25   22
+Paris   27.5   28   27
+Rome    30.0   30   30
+      0            1
+0  www.      example
+1  www.  anothersite
+2   NaN      example
+0      great movie! #mustwatch
+1    not bad, could be better.
+2      terrible acting, avoid!
+dtype: object
+0         apple
+1        banana
+2        orange
+3    grapefruit
+dtype: object
+a    10
+b    20
+c    30
+d    40
+e    50
+dtype: int64
+Bob        NaN
+Alice      NaN
+Charlie    NaN
+dtype: object
+
+Bob        Charlie
+Alice      Charlie
+Charlie    Charlie
+dtype: object
+Bob          Alice
+Alice          Bob
+Charlie    Charlie
+dtype: object
